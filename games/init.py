2533f5831b07d8a365b2a6f288aa46c37692a00f
عبير <alqaws151@gmail.com>
2025-11-13 17:32:45 +0300

    init.py

---
+++ games/init.py
@@ -0,0 +1,16 @@
+"""
+حزمة الألعاب
+"""
+from .compatibility_game import CompatibilityGame
+
+# يمكن إضافة المزيد من الألعاب هنا عند تطويرها
+# from .chain_words_game import ChainWordsGame
+# from .fast_typing_game import FastTypingGame
+# ...
+
+__all__ = [
+    'CompatibilityGame',
+    # 'ChainWordsGame',
+    # 'FastTypingGame',
+    # ...
+]
