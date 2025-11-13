9e2129623d01fe2008cb28e5f90c5e50a4c62342
عبير <alqaws151@gmail.com>
2025-11-13 17:31:14 +0300

    __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,28 @@
 """
-Utils Package - الوظائف المساعدة للبوت
+حزمة الأدوات المساعدة
 """
+from .ui_components import *
+from .helpers import *
+from .database import *
+from .gemini_config import *
 
-print("✅ utils package loaded successfully")
-
-__version__ = "1.0.0"
+__all__ = [
+    'get_welcome_message',
+    'get_help_message',
+    'get_join_message',
+    'get_withdrawal_message',
+    'get_error_message',
+    'get_success_message',
+    'get_fixed_quick_reply',
+    'normalize_text',
+    'load_lines_from_file',
+    'get_random_line',
+    'init_db',
+    'add_player',
+    'get_player',
+    'update_player_score',
+    'get_leaderboard',
+    'USE_AI',
+    'get_gemini_api_key',
+    'switch_gemini_key'
+]
