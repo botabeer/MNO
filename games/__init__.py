80ff138778960f56e77a579f0ebad6bfd8f13907
عبير <alqaws151@gmail.com>
2025-11-14 03:40:10 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,16 +1,24 @@
 """
-حزمة الألعاب
+Games Package
+Contains all game implementations for the LINE Bot
 """
-from .compatibility_game import CompatibilityGame
 
-# يمكن إضافة المزيد من الألعاب هنا عند تطويرها
-# from .chain_words_game import ChainWordsGame
-# from .fast_typing_game import FastTypingGame
-# ...
+from .song_game import SongGame
+from .opposite_game import OppositeGame
+from .compatibility_game import CompatibilityGame
+from .differences_game import DifferencesGame
+from .fast_typing_game import FastTypingGame
+from .chain_words_game import ChainWordsGame
+from .human_animal_plant_game import HumanAnimalPlantGame
+from .letters_words_game import LettersWordsGame
 
 __all__ = [
+    'SongGame',
+    'OppositeGame',
     'CompatibilityGame',
-    # 'ChainWordsGame',
-    # 'FastTypingGame',
-    # ...
+    'DifferencesGame',
+    'FastTypingGame',
+    'ChainWordsGame',
+    'HumanAnimalPlantGame',
+    'LettersWordsGame'
 ]
