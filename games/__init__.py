bed9c5385780d1ef88cd9ef31b851d98b718d9f6
عبير <alqaws151@gmail.com>
2025-12-02 00:25:58 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,26 +1,10 @@
-"""
-Games Package
-Contains all game implementations for the LINE Bot
-"""
-
 from .song_game import SongGame
 from .opposite_game import OppositeGame
 from .compatibility_game import CompatibilityGame
-from .differences_game import DifferencesGame
 from .fast_typing_game import FastTypingGame
 from .chain_words_game import ChainWordsGame
 from .human_animal_plant_game import HumanAnimalPlantGame
 from .letters_words_game import LettersWordsGame
 from .mafia_game import MafiaGame
 
-__all__ = [
-    'SongGame',
-    'OppositeGame',
-    'CompatibilityGame',
-    'DifferencesGame',
-    'FastTypingGame',
-    'ChainWordsGame',
-    'HumanAnimalPlantGame',
-    'LettersWordsGame',
-    'MafiaGame'
-]
+__all__ = ['SongGame', 'OppositeGame', 'CompatibilityGame', 'FastTypingGame', 'ChainWordsGame', 'HumanAnimalPlantGame', 'LettersWordsGame', 'MafiaGame']
