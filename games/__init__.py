c9a40c3be43bd28c5d7c0e224d5b4692fe3463c3
عبير <alqaws151@gmail.com>
2025-12-02 01:49:34 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -5,6 +5,7 @@ from .fast_typing_game import FastTypingGame
 from .chain_words_game import ChainWordsGame
 from .human_animal_plant_game import HumanAnimalPlantGame
 from .letters_words_game import LettersWordsGame
+from .category_letter_game import CategoryLetterGame
 from .mafia_game import MafiaGame
 
-__all__ = ['SongGame', 'OppositeGame', 'CompatibilityGame', 'FastTypingGame', 'ChainWordsGame', 'HumanAnimalPlantGame', 'LettersWordsGame', 'MafiaGame']
+__all__ = ['SongGame', 'OppositeGame', 'CompatibilityGame', 'FastTypingGame', 'ChainWordsGame', 'HumanAnimalPlantGame', 'LettersWordsGame', 'CategoryLetterGame', 'MafiaGame']
