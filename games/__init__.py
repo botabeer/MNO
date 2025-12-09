7d8138a14f47138e6a87edfe82e64237477d65f1
عبير <alqaws151@gmail.com>
2025-12-09 04:40:48 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,8 +1,7 @@
 # games/__init__.py
-from .constants import *
 from .game_manager import GameManager
 from .game_helpers import *
-from .category_letter_game import CategoryLetterGame
+from .seen_jeem_game import SeenJeemGame
 from .chain_words_game import ChainWordsGame
 from .compatibility_game import CompatibilityGame
 from .fast_typing_game import FastTypingGame
@@ -11,14 +10,14 @@ from .letters_words_game import LettersWordsGame
 from .mafia_game import MafiaGame
 from .opposite_game import OppositeGame
 from .song_game import SongGame
+from .loreet_game import LoreetGame
 
 __all__ = [
     "GameManager",
     "normalize_text", "create_game_header", "create_progress_box",
     "create_separator", "create_action_buttons", "create_winner_card",
-    "create_hint_text",
-    "COLORS", "MAFIA_CONFIG",
-    "CategoryLetterGame", "ChainWordsGame", "CompatibilityGame",
+    "create_hint_text", "create_question_card",
+    "SeenJeemGame", "ChainWordsGame", "CompatibilityGame",
     "FastTypingGame", "HumanAnimalPlantGame", "LettersWordsGame",
-    "MafiaGame", "OppositeGame", "SongGame"
+    "MafiaGame", "OppositeGame", "SongGame", "LoreetGame"
 ]
