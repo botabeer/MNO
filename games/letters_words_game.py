487372be8d6a2f273bc128238c246f4f5f042d7e
عبير <alqaws151@gmail.com>
2025-12-11 04:09:33 +0300

    letters_words_game.py

--- games/letters_words_game.py
+++ games/letters_words_game.py
@@ -25,47 +25,7 @@ class LettersWordsGame:
             {"letters": "ن و ر س م ا", "answers": ["نور", "سمر", "مان", "سور", "نار", "رمس", "مرس", "روس", "سمن", "نوم"]},
             {"letters": "ف ل ج ر ب ح", "answers": ["فجر", "جرح", "حرب", "حفل", "فلج", "برج", "رحب", "جفل", "فرح", "لحب"]},
             {"letters": "س ل ا م و ن", "answers": ["سلام", "سلم", "مان", "سما", "لوم", "ماس", "سول", "نام", "نسل", "ملس"]},
-            {"letters": "ع ل ي ا ن ب", "answers": ["علي", "عليا", "بني", "ليان", "بان", "بعل", "نيل", "عني", "نبي", "علن"]},
-            {"letters": "ص ب ح ا ل ر", "answers": ["صباح", "حصار", "صبر", "بحر", "صحار", "حار", "صحر", "برح", "صلح", "حبل"]},
-            {"letters": "ج م ي ل ا ه", "answers": ["جميل", "جميله", "ليل", "جمال", "ملاح", "حمل", "جال", "ملح", "ليم", "حلم"]},
-            {"letters": "ط ف ل و ه ا", "answers": ["طفل", "طفله", "طول", "طوف", "فول", "لطف", "لوف", "طاف", "فله", "طلو"]},
-            {"letters": "ق ر ا ن ء ب", "answers": ["قران", "نار", "قار", "نقر", "رقن", "قرن", "انر", "نرق", "قرب", "برق"]},
-            {"letters": "ع ا ل م ي ه", "answers": ["عالم", "عالمي", "علم", "معلم", "ملح", "علي", "حمل", "عمل", "ملي", "حلم"]},
-            {"letters": "ش م س ر و ق", "answers": ["شمس", "شرق", "مشروق", "سور", "شور", "قرش", "قوس", "سقر", "روش", "مرق"]},
-            {"letters": "ج ب ل ا ر ه", "answers": ["جبل", "جبار", "جار", "برج", "بحر", "جره", "بار", "رجل", "جرب", "لهب"]},
-            {"letters": "ق م ر ن و ج", "answers": ["قمر", "نجم", "نور", "قرن", "جرن", "مرج", "رجم", "قرم", "جمر", "رمق"]},
-            {"letters": "ب ح ر ا ن ي", "answers": ["بحر", "بحري", "بحار", "بحران", "نار", "حرب", "حبر", "ناري", "حير", "بني"]},
-            {"letters": "ن خ ل ا ج ز", "answers": ["نخل", "جزء", "نجز", "خزن", "زجل", "حزن", "لخن", "جزل", "نزل", "خجل"]},
-            {"letters": "س و ق ل ب ا", "answers": ["سوق", "قلب", "باس", "سبل", "قبل", "بلا", "لبس", "سقل", "لقب", "سلب"]},
-            {"letters": "ز ه ر و د ق", "answers": ["زهر", "زهور", "ورد", "ورده", "دور", "زور", "قرد", "دهر", "زرد", "هدر"]},
-            {"letters": "ح ك م ه ل ا", "answers": ["حكم", "حكمه", "حلم", "ملح", "كلم", "حمل", "لحم", "كمل", "حلك", "لمح"]},
-            {"letters": "ش ع ر ي ب ا", "answers": ["شعر", "شعري", "عرب", "بشر", "رشي", "بعر", "يشع", "ربع", "شري", "عري"]},
-            {"letters": "ط ر ي ق ل ه", "answers": ["طريق", "طريقه", "قرط", "طير", "قرل", "طلق", "قلط", "طرق", "قري", "لطي"]},
-            {"letters": "ص د ي ق ا ح", "answers": ["صديق", "حصاد", "صاد", "حديق", "صيد", "حديد", "صدق", "حقد", "قصد", "صحي"]},
-            {"letters": "ت ر ا ب و م", "answers": ["تراب", "بوم", "ترم", "بات", "توم", "رمت", "بار", "روم", "متر", "مرت"]},
-            {"letters": "ن ج و م ي ل", "answers": ["نجوم", "جمل", "جول", "نيل", "لجم", "ملي", "ليم", "مول", "جمي", "نجل"]},
-            {"letters": "ق ص ر ا ي ع", "answers": ["قصر", "قصير", "عصر", "عصير", "قرص", "صعر", "رصع", "عرق", "قري", "صري"]},
-            {"letters": "ه و ا ء ل ي", "answers": ["هواء", "ليل", "لي", "هوى", "اول", "ولي", "الي", "يول", "هلي", "ليه"]},
-            {"letters": "خ ب ز ا ر ي", "answers": ["خبز", "خبار", "بخار", "خزي", "ريخ", "برخ", "زير", "خري", "بزر", "زبر"]},
-            {"letters": "ف ر ح ا ن ب", "answers": ["فرح", "فرحان", "حران", "فان", "برح", "حفر", "نبح", "رحب", "فرن", "حرف"]},
-            {"letters": "ص ف ا ء ل و", "answers": ["صفاء", "صفو", "صول", "فصل", "فول", "صلف", "وفا", "لصف", "فلص", "صلو"]},
-            {"letters": "ح ي ا ت ن ف", "answers": ["حياه", "حيات", "فني", "نفي", "حني", "فتن", "يفن", "حتف", "تحي", "نحت"]},
-            {"letters": "ج و ه ر ا ب", "answers": ["جوهر", "جار", "جهر", "هجر", "برج", "جور", "رجو", "جهاب", "جرب", "رجب"]},
-            {"letters": "م س ج د ا ر", "answers": ["مسجد", "مدرس", "سدر", "رمس", "سجر", "جسم", "مجس", "رجس", "سمد", "جرس"]},
-            {"letters": "ن ا س ل ي ب", "answers": ["ناس", "ليبيا", "بني", "سيل", "بان", "سني", "نيل", "باس", "نسل", "ليس"]},
-            {"letters": "ع ز ي ز ا ه", "answers": ["عزيز", "عزه", "زها", "عزا", "زيه", "يزع", "عيز", "هزع", "عزي", "زعي"]},
-            {"letters": "غ ر ي ب ا ن", "answers": ["غريب", "غار", "غبار", "نار", "برن", "غبي", "بين", "غرن", "غرب", "ريغ"]},
-            {"letters": "ك ر ي م ه ل", "answers": ["كريم", "كريمه", "حليم", "حلم", "ملك", "ليم", "كلم", "مكر", "كمل", "لكم"]},
-            {"letters": "س ر ي ع ا ب", "answers": ["سريع", "عصر", "سبع", "بعر", "سير", "رعي", "عبر", "رسع", "سعي", "عير"]},
-            {"letters": "ق و ي ا ن ه", "answers": ["قوي", "قوه", "نقي", "يقن", "قون", "هون", "يوق", "قنو", "نوي", "قين"]},
-            {"letters": "ض ع ي ف ا ر", "answers": ["ضعيف", "رفع", "عرف", "فرع", "ضفر", "فرض", "عفر", "رضع", "ضرع", "فعر"]},
-            {"letters": "ح ل و ا ي ب", "answers": ["حلو", "حلوى", "حوب", "بلو", "حوي", "ليح", "يحل", "بول", "حيل", "لوح"]},
-            {"letters": "م ر ا ح ي ل", "answers": ["مرح", "حرام", "رحيل", "ملح", "حمل", "ريح", "لحم", "حرم", "رحم", "لمح"]},
-            {"letters": "س م ك ا ع ر", "answers": ["سمك", "عسكر", "سكر", "عرك", "سمع", "كسر", "عرم", "سرع", "كرم", "سعر"]},
-            {"letters": "ط ي ر ا ن ب", "answers": ["طيران", "طير", "بري", "طين", "رين", "نبت", "طرن", "برن", "طرب", "نطر"]},
-            {"letters": "ج ز ي ر ه ا", "answers": ["جزيره", "جزير", "جره", "زهر", "رزي", "جري", "زير", "رجي", "جرز", "هزر"]},
-            {"letters": "ق ل ع ه ا ت", "answers": ["قلعه", "علقه", "عقل", "قعل", "علت", "قلت", "عتل", "تعل", "لقع", "قعت"]},
-            {"letters": "م د ي ن ه ا", "answers": ["مدينه", "مدين", "ديم", "يمن", "منه", "نمي", "دين", "ميد", "دمن", "همد"]}
+            {"letters": "ع ل ي ا ن ب", "answers": ["علي", "عليا", "بني", "ليان", "بان", "بعل", "نيل", "عني", "نبي", "علن"]}
         ]
         self.questions = []
         self.current_question = 0
@@ -74,12 +34,14 @@ class LettersWordsGame:
         self.found_words = {}
         self.valid_words = []
         self.words_needed = 3
+        self.first_correct_answer = False
 
     def start_game(self):
         self.questions = random.sample(self.challenges, self.total_questions)
         self.current_question = 0
         self.player_scores = {}
         self.found_words = {}
+        self.first_correct_answer = False
         return self._show_question()
 
     def _show_question(self):
@@ -87,6 +49,7 @@ class LettersWordsGame:
         letters = challenge['letters']
         progress = f"{self.current_question + 1}/{self.total_questions}"
         self.valid_words = [normalize_text(word) for word in challenge['answers']]
+        self.first_correct_answer = False
         
         return FlexSendMessage(
             alt_text="تكوين الكلمات",
@@ -114,6 +77,7 @@ class LettersWordsGame:
         self.current_question += 1
         if self.current_question < self.total_questions:
             self.found_words = {}
+            self.first_correct_answer = False
             return self._show_question()
         return None
 
@@ -126,6 +90,7 @@ class LettersWordsGame:
 
         if text.lower() in ['جاوب', 'الحل']:
             some_words = ' - '.join(self.questions[self.current_question]['answers'][:5])
+            self.first_correct_answer = True
             if self.current_question + 1 < self.total_questions:
                 return {'response': TextSendMessage(text=f"بعض الكلمات الصحيحة\n{some_words}"), 'points': 0, 'correct': False, 'next_question': True}
             return self._end_game()
@@ -148,6 +113,7 @@ class LettersWordsGame:
         words_count = len(self.found_words[user_id])
 
         if words_count >= self.words_needed:
+            self.first_correct_answer = True
             if self.current_question + 1 < self.total_questions:
                 return {'response': TextSendMessage(text=f"اجابة صحيحة {display_name} +{points} نقطة"), 'points': points, 'correct': True, 'won': True, 'next_question': True}
             return self._end_game()
